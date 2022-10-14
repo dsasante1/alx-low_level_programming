fb0bcd37cc195d13ba6423237dd6ebb63419afba
ds asante <114578080+dsasante1@users.noreply.github.com>
2022-10-14 13:36:45 +0000

    trailing whitespaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/* more headers goes there */
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
@@ -14,12 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
-
-/** still whitespaces
