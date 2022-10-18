19726800984d3f16f5ac499baec67f454a97b6bc
K Asante <114578080+dsasante1@users.noreply.github.com>
2022-10-18 17:46:36 +0000

    create putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("_putchar");
+return (0);
+}
