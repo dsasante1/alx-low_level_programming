eca381e5a75d3e7e3ad0838b2ce7b1a9c3b0de33
K Asante <114578080+dsasante1@users.noreply.github.com>
2022-10-26 19:02:35 +0000

    update 1-3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * _strncpy - copy string
- * @src : pointer of a char
- * @dest : pointer of char
- * @n : integer
- * Return : char
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && src[i] != '\0')
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	if (i < n)
+	while (i < n)
 	{
-		for (; i < n; i++)
-			dest[i] = '\0';
+		dest[i] = '\0';
+		i++;
 	}
+
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * _strcmp - function comparison of strings
- * @s1 : pointer
- * @s2 pointer
- * Return: j-integer
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	j = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 == '\0')
 		{
-		j = s1[i] - s2[i];
-		break;
+			return (0);
 		}
+		s1++;
+		s2++;
 	}
-	return (j);
+	return (*s1 - *s2);
 }
